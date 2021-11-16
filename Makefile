b410182c6851d28c6b9a978d833c1e6a2ac5083e
Steve McDougall <juststevemcd@gmail.com>
2021-11-16 19:39:54 +0000

    Laravel working with traefik in docker for sub-domain routing

---
+++ Makefile
@@ -0,0 +1,32 @@
+.RECIPEPREFIX +=
+.DEFAULT_GOAL := help
+
+help:
+	@echo "Welcome to IT Support, have you tried turning it off and on again?"
+
+install:
+	@composer install
+
+test:
+	docker exec crm_php php artisan test
+
+migrate:
+	docker exec crm_php php artisan migrate
+
+analyse:
+	./vendor/bin/phpstan analyse
+
+generate:
+	docker exec crm_php php artisan ide-helper:models --write
+
+nginx:
+	docker exec -it crm_nginx /bin/sh
+
+php:
+	docker exec -it crm_php /bin/sh
+
+mysql:
+	docker exec -it crm_mysql /bin/sh
+
+redis:
+	docker exec -it crm_redis /bin/sh
---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string
+     */
+    public const HOME = '/home';
+
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->configureRateLimiting();
+
+        $this->routes(function () {
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+
+    /**
+     * @return void
+     */
+    protected function configureRateLimiting(): void
+    {
+        RateLimiter::for('api', fn (Request $request) =>
+            /** @phpstan-ignore-next-line  */
+            Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip())
+        );
+    }
+}
